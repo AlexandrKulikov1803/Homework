3281fbebe9b6617c12ffad113750b86aa86fc5e4
Alexandr Kulikov <kulic1803@yandex.ru>
2024-10-02 03:54:48 +0600

    Adding past homework

---
+++ src/masks.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str, int]) -> str:
+    """Функция маскировки номера банковской карты"""
+
+    hidden_card_number = ""
+    card_number = str(card_number)
+    for i, char in enumerate(card_number):
+        if i in range(6, 12):
+            hidden_card_number += "*"
+        else:
+            hidden_card_number += char
+
+    list_number = [
+        hidden_card_number[0:4],
+        hidden_card_number[4:8],
+        hidden_card_number[8:12],
+        hidden_card_number[12:16],
+    ]
+    mask_card_number = " ".join(list_number)
+
+    return mask_card_number
+
+
+def get_mask_account(account: Union[str, int]) -> str:
+    """Функция маскировки номера банковского счёта"""
+
+    account = str(account)
+    mask_account = "**" + account[-4:]
+    return mask_account
