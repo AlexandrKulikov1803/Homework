c6fb366d0faa02730d0fd1c072fd98d4fecc5fad
Alexandr Kulikov <kulic1803@yandex.ru>
2024-10-02 12:02:57 +0600

    Checking linters

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,10 @@
-from .masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card_info: str) -> str:
     """Функция маскировки номера карты/счёта"""
     if "Счет" in account_card_info:
-        account_info_mask = ''
+        account_info_mask = ""
         for i, char in enumerate(account_card_info):
             if char in "0123456789":
                 account_info_mask += get_mask_account(account_card_info[i:])
@@ -13,7 +13,7 @@ def mask_account_card(account_card_info: str) -> str:
                 account_info_mask += char
         return account_info_mask
     else:
-        card_info_mask = ''
+        card_info_mask = ""
         for i, char in enumerate(account_card_info):
             if char in "0123456789":
                 card_info_mask += get_mask_card_number(account_card_info[i:])
@@ -27,5 +27,5 @@ def get_date(exact_date: str) -> str:
     day = exact_date[8:10]
     month = exact_date[5:7]
     year = exact_date[0:4]
-    date_format_day_month_year = day + '.' + month + '.' + year
+    date_format_day_month_year = day + "." + month + "." + year
     return date_format_day_month_year
