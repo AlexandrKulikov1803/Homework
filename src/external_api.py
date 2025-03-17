e2c6a1c508b21802e7e06f256ee79d20b279a34d
Alexandr Kulikov <kulic1803@yandex.ru>
2025-03-18 01:59:26 +0600

    Adding files: utils, external_api

---
+++ src/external_api.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+
+def currency_conversion(transaction: dict) -> float:
+    """Функция, которая принимает на вход путь до JSON-файла и
+    возвращает список словарей с данными о финансовых транзакциях."""
+    code = transaction["operationAmount"]["currency"]["code"]
+    amount = float(transaction["operationAmount"]["amount"])
+    if code != "RUB":
+        headers = {"apikey": api_key}
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}"
+        conversion_to_RUB = requests.get(url, headers=headers)
+        print(conversion_to_RUB.status_code)
+        amount = conversion_to_RUB.json()["result"]
+    return amount
