d9809b96d00f3f86f759be277343f909e2ca8329
Alexandr Kulikov <kulic1803@yandex.ru>
2025-04-27 03:37:22 +0600

    Fixed files:generators.py, test_generators.py and conftest.py

--- src/generators.py
+++ src/generators.py
@@ -3,8 +3,14 @@ from typing import Generator
 
 def filter_by_currency(list_transactions: list, currency: str) -> Generator[dict, None, None]:
     """Функция, возвращающая итератор, который поочередно выдает транзакции по заданной валюте"""
+    if list_transactions[0].get("operationAmount"):
+        list_transactions = [x for x in list_transactions if x]
+        filtered_transactions = [x for x in list_transactions if
+                                 x.get("operationAmount")["currency"]["code"] == currency]
 
-    filtered_transactions = [x for x in list_transactions if x["operationAmount"]["currency"]["code"] == currency]
+    elif list_transactions[0].get("currency_code"):
+        filtered_transactions = [x for x in list_transactions if x.get("currency_code") == currency]
+
     count = 0
     while count != len(filtered_transactions):
         yield filtered_transactions[count]
@@ -25,7 +31,7 @@ def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """Функция, принимающая начальное и конечное значения для генерации диапазона номеров."""
     if start <= 0 or end <= 0:
         yield "Введено неверное значение"
-    elif start < 10**16 and end < 10**16:
+    elif start < 10 ** 16 and end < 10 ** 16:
         count = start
         while count <= end:
             str_number = "0" * (16 - len(str(count))) + str(count)
